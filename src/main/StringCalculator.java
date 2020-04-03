173f3d08591d11ac8c6636843ec71071cec91702
Steve Lydford <steve@codurance.com>
2020-04-03 14:24:25 +0100

    Allow custom separators and disallow negative numbers

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -2,19 +2,41 @@ package main;
 
 public class StringCalculator {
 
-    public int add(String number) {
+    public int add(String number) throws Exception {
         if(number == "")
             return 0;
 
-        String[] numbers = splitByNewlineAndComma(number);
+        String[] numbers;
+
+        if(number.startsWith("//")){
+            numbers = splitByCustomSeparator(number);
+        }
+        else {
+            numbers = splitByNewlineAndComma(number);
+        }
 
         int total = 0;
+        String negativeNumbers = "";
+
         for (int i = 0; i < numbers.length; i++)
         {
+            if(Integer.parseInt(numbers[i]) < 0)
+            {
+                negativeNumbers += " " + Integer.parseInt(numbers[i]);
+            }
             total += Integer.parseInt(numbers[i]);
         }
 
+        if(negativeNumbers.length() > 0)
+            throw new IllegalArgumentException("negatives not allowed:" + negativeNumbers);
+
         return total;
+    }
+
+    private String[] splitByCustomSeparator(String number) {
+        char customSeparator = number.charAt(2);
+        number = number.substring(4);
+        return number.split("[\n" + customSeparator + "]");
     }
 
     private String[] splitByNewlineAndComma(String number) {
--- src/test/StringCalculatorMust.java
+++ src/test/StringCalculatorMust.java
@@ -3,7 +3,9 @@ package test;
 import main.StringCalculator;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class StringCalculatorMust {
 
@@ -15,27 +17,47 @@ public class StringCalculatorMust {
     }
 
     @Test
-    public void returnZeroForEmptyString(){
+    public void returnZeroForEmptyString() throws Exception {
         Assert.assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    public void returnParseIntForSingleNumber(){
+    public void returnParseIntForSingleNumber() throws Exception {
         Assert.assertEquals(4, stringCalculator.add("4"));
     }
 
     @Test
-    public void returnTheSumOfTwoNumber(){
+    public void returnTheSumOfTwoNumber() throws Exception {
         Assert.assertEquals(3, stringCalculator.add("1,2"));
     }
 
     @Test
-    public void returnTheSumOfMultipleNumbers(){
+    public void returnTheSumOfMultipleNumbers() throws Exception {
         Assert.assertEquals(45, stringCalculator.add("1,2,3,4,5,6,7,8,9"));
     }
 
     @Test
-    public void handleNewlinesAsSeparators(){
+    public void handleNewlinesAsSeparators() throws Exception {
         Assert.assertEquals(6, stringCalculator.add("1\n2,3"));
+    }
+
+    @Test
+    public void handleCustomSeparators() throws Exception {
+        Assert.assertEquals(3, stringCalculator.add("//;\n1;2"));
+    }
+
+    @Test
+    public void handleCustomArbitrarySeparators() throws Exception {
+        Assert.assertEquals(3, stringCalculator.add("//*\n1*2"));
+    }
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Test
+    public void disallowNegativeNumbers() throws Exception {
+        exceptionRule.expect(IllegalArgumentException.class);
+        exceptionRule.expectMessage("negatives not allowed: -2 -3");
+        stringCalculator.add("1,-2,-3");
     }
 }
